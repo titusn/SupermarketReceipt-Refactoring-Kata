8c342cca47a2125b6a669dcf6852fa2c2bf77f15
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-04-14 10:54:56 +0200

    Duplicate methods in abstract class for refactoring

--- java/src/main/java/dojo/supermarket/model/SpecialOffer.java
+++ java/src/main/java/dojo/supermarket/model/SpecialOffer.java
@@ -3,4 +3,19 @@ package dojo.supermarket.model;
 abstract class SpecialOffer {
     abstract String generateDescription(Double amount);
     abstract Double calculateDiscountAmount(double quantity, double offerAmount, double unitPrice);
+
+    protected double calculateDiscountAmount(double quantity, double offerPrice, double unitPrice, int divisor) {
+        return calculateDiscountAmount(quantity, offerPrice, unitPrice, divisor, 1);
+    }
+
+    protected double calculateDiscountAmount(double quantity, double offerPrice, double unitPrice, int divisor, int multiplier) {
+        double subtotalDiscounted = offerPrice * getNumberOfTimesOfferApplies((int) quantity, divisor) * multiplier;
+        double subtotalAboveDiscount = ((int) quantity % divisor) * unitPrice;
+        double total = subtotalDiscounted + subtotalAboveDiscount;
+        return unitPrice * quantity - total;
+    }
+
+    protected int getNumberOfTimesOfferApplies(int quantityAsInt, int divisor) {
+        return quantityAsInt / divisor;
+    }
 }
